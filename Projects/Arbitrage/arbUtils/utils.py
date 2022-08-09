db1d401f2ce8b56bea4daae5e7d3c8f4e226559c
bradleyboyuyang <119020065@link.cuhk.edu.cn>
2022-08-09 16:10:37 +0800

    update

---
+++ Projects/Arbitrage/arbUtils/utils.py
@@ -0,0 +1,42 @@
+from difflib import restore
+from multiprocessing.spawn import prepare
+from typing import get_origin
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import statsmodels.api as sm
+import scipy.stats as stats
+from datetime import datetime
+from datetime import timedelta
+import os
+import glob
+import gc
+import dask
+import joblib
+from multiprocessing import cpu_count
+import functools
+from itertools import product
+from collections import OrderedDict, defaultdict, Counter
+import warnings
+import hfhd.hf as hf
+from tqdm import tqdm, trange
+import pickle
+import redis
+from arbUtils.RemoteQuery import *
+
+warnings.filterwarnings("ignore")
+pd.set_option('expand_frame_repr', False)
+pd.set_option('display.unicode.ambiguous_as_wide', True)
+pd.set_option('display.unicode.east_asian_width', True)
+pd.set_option('display.width', 180)
+pd.set_option('display.max_columns', None)
+
+import torch
+
+class ArbitrageUtils:
+    def __init__(self):
+        self.src = RemoteSrc()
+        self.pool = redis.ConnectionPool(host='localhost', port=6379, decode_responses=False)
+        self.server = redis.Redis(connection_pool=self.pool)
+
